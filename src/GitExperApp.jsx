e5e7b0c0cdcccf0d63b28fada8f5b9e26024bfd2
Lutejada <luetejava@gmail.com>
2023-01-14 22:53:17 -0500

    primer hook

--- src/GitExperApp.jsx
+++ src/GitExperApp.jsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
 
 export const GitExperApp = () => {
+
+    const [categories, setCategories] = useState([ 'One Punch', 'Dragon ball'] );
+
+    console.log( categories )
+
   return (
     <>
       <h1>GitExpertApp</h1>
+
+      <ol>
+        {
+            categories.map( category => {
+                return  <li key={ category} > { category } </li>
+            })
+        }
+
+      </ol>
     </>
   )
 };
